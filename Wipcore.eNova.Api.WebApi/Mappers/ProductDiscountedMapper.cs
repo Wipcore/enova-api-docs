053a5da487a6aed40a4f36e5f4af768500df3211
Henrik Eliasson <henrik.eliasson@wipcore.se>
2016-02-29 10:57:40 +0100

    API. Changed discount mapper to look at pricelist property IsDefault

--- Wipcore.eNova.Api.WebApi/Mappers/ProductDiscountedMapper.cs
+++ Wipcore.eNova.Api.WebApi/Mappers/ProductDiscountedMapper.cs
@@ -3,41 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Wipcore.Core;
 using Wipcore.Core.SessionObjects;
 using Wipcore.Enova.Api.Interfaces;
+using Wipcore.Enova.Api.WebApi.Helpers;
 using Wipcore.Enova.Core;
 using Wipcore.Enova.Generics;
+using MapType = Wipcore.Enova.Api.Interfaces.MapType;
 
 namespace Wipcore.eNova.Api.WebApi.Mappers
 {
     public class ProductDiscountedMapper : IPropertyMapper
     {
-        private readonly IConfigurationRoot _configuration;
-        
-
-        public ProductDiscountedMapper(IConfigurationRoot configuration)
-        {
-            _configuration = configuration;
-        }
-
         public List<string> Names  => new List<string>() {"IsDiscounted", "ListPriceExclTax", "ListPriceInclTax"};
         public Type Type => typeof (EnovaBaseProduct);
         public bool InheritMapper => true;
+        
+
         public int Priority => 0;
         public MapType MapType => MapType.MapFrom;
 
-        public object MapTo(BaseObject obj, string propertyName)
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public object MapFrom(BaseObject obj, string propertyName)
         {
             var context = obj.GetContext();
-            var currentCurrencyIdentifier = context.CurrentCurrency?.Identifier;
-            var defaultPriceListIdentifier = _configuration.GetSection("api")?.GetSection("defaultpricelist")?.GetChildren()?.
-                FirstOrDefault(x => String.Equals(x.Key, currentCurrencyIdentifier, StringComparison.InvariantCultureIgnoreCase))?.Value;
-            var defaultPriceList = context.FindObject<EnovaPriceList>(defaultPriceListIdentifier);
+            var currentCurrencyId = context.CurrentCurrency?.ID ?? -1;
+            var defaultPriceList = context.Search<EnovaPriceList>("IsDefault = true AND CurrencyID = "+currentCurrencyId).FirstOrDefault();
             var product = (EnovaBaseProduct)obj;
 
             if (defaultPriceList == null || !defaultPriceList.HasProduct(product))
@@ -51,6 +42,11 @@ namespace Wipcore.eNova.Api.WebApi.Mappers
                 return currentPrice < defaultPrice;
 
             return defaultPrice;
+        }
+
+        public object MapTo(BaseObject obj, string propertyName)
+        {
+            throw new NotImplementedException();
         }
     }
 }
