dc2ebfad4a03dd820ad896d3efad6e9012ef6f91
fredrik.astrand <fredrik.astrand@wipcore.se>
2016-03-10 10:51:20 +0100

    Refactoring and bugfixes.

---
+++ Wipcore.eNova.Api.Models/Interfaces/IOrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Wipcore.Enova.Api.Models.Interfaces
+{
+    public interface IOrderModel
+    {
+        IEnumerable<IOrderRowModel> Rows { get; set; }
+        string Name { get; set; }
+        string Identifier { get; set; }
+        string Customer { get; set; }
+        decimal TotalPriceInclTax { get; set; }
+        decimal TotalPriceExclTax { get; set; }
+        IDictionary<string, object> AdditionalValues { get; set; }
+    }
+}
