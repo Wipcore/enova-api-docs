226c24f62c9c54e3809838f19a8bf1e120eba567
Henrik Eliasson <henrik.eliasson@wipcore.se>
2016-04-04 15:41:23 +0200

    Added basic section controller, for enovaproductsections.

---
+++ Wipcore.eNova.Api.WebApi/Controllers/SectionsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Mvc;
+using Wipcore.Enova.Api.Interfaces;
+using Wipcore.Enova.Api.Models;
+using Wipcore.Enova.Core;
+
+namespace Wipcore.eNova.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Route("api/{market}/[controller]")]
+    public class SectionsController : ApiController
+    {
+        private readonly IObjectService _objectService;
+        private readonly IContextService _contextService;
+
+        public SectionsController(IObjectService objectService, IContextService contextService)
+        {
+            _objectService = objectService;
+            _contextService = contextService;
+        }
+
+        [HttpGet(/*"{location}"*/)]
+        public IEnumerable<IDictionary<string, object>> Get([FromUri] ContextModel requestContext, [FromUri] GetParametersModel getParameters)
+        {
+            return _objectService.Get<EnovaBaseProductSection>(requestContext, getParameters);
+        }
+
+        [HttpGet("{identifier}")]
+        public IDictionary<string, object> Get(ContextModel requestContext, GetParametersModel getParameters, string identifier)
+        {
+            return _objectService.Get<EnovaBaseProductSection>(requestContext, getParameters, identifier);
+        }
+
+        [HttpGet("{identifier}/products")]
+        public IEnumerable<IDictionary<string, object>> GetProducts(ContextModel requestContext, GetParametersModel getParameters, string identifier)
+        {
+            var context = _contextService.GetContext();
+            var section = EnovaBaseProductSection.Find(context, identifier);
+            var items = section.GetItems(typeof (EnovaBaseProduct));
+            return _objectService.Get<EnovaBaseProduct>(requestContext, getParameters, items);
+        }
+    }
+}
