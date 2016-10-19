df89610ebc9035f55f2fb62dc746326a73a37639
Henrik Eliasson <henrik.eliasson@wipcore.se>
2016-10-19 10:52:45 +0200

    Added promo mappers to api

---
+++ src/Wipcore.Enova.Api.WebApi/Mappers/Promo/PromoConditionsMapper.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Wipcore.Core.SessionObjects;
+using Wipcore.Enova.Api.Interfaces;
+using Wipcore.Enova.Api.WebApi.Helpers;
+using Wipcore.Enova.Core;
+using Wipcore.Enova.Generics;
+
+namespace Wipcore.eNova.Api.WebApi.Mappers.Promo
+{
+    public class PromoConditionsMapper : IPropertyMapper
+    {
+        public List<string> Names => new List<string>() { "Conditions" };
+        public Type Type => typeof(EnovaPromo);
+        public bool InheritMapper => true;
+
+        public int Priority => 0;
+        public MapType MapType => MapType.MapFromAndToEnovaAllowed;
+
+        public object MapFromEnovaProperty(BaseObject obj, string propertyName)
+        {
+            var promo = (EnovaPromo) obj;
+            var amountCondition = promo.GetConditions(typeof (EnovaOrderAmountPromoCondition)).Cast<EnovaOrderAmountPromoCondition>().FirstOrDefault();
+            var productCondition = promo.GetConditions(typeof(EnovaProductQuantityPromoCondition)).Cast<EnovaProductQuantityPromoCondition>().FirstOrDefault();
+            var amountCategoryCondition = promo.GetConditions(typeof(EnovaSectionAmountPromoCondition)).Cast<EnovaSectionAmountPromoCondition>().FirstOrDefault();
+            var categoryCondition = promo.GetConditions(typeof(EnovaSectionQuantityPromoCondition)).Cast<EnovaSectionQuantityPromoCondition>().FirstOrDefault();
+
+            Currency amountCurrency = null;
+            int decimals;
+            var amount = amountCondition == null ? 0m : amountCondition.GetAmount(out decimals, ref amountCurrency);
+
+            Currency categoryAountCurrency = null;
+            var categoryAmount = amountCategoryCondition == null ? 0m : amountCategoryCondition.GetAmount(out decimals, ref categoryAountCurrency);
+
+            var conditionsModel = new
+            {
+                BuyFor = amountCondition != null,
+                BuyForAmount = amount,
+                BuyForAmountCurrency = amountCurrency?.Identifier,
+                BuyForAmountIncludeTax = amountCondition?.TaxIncluded == true,
+
+                BuyProduct = productCondition != null,
+                BuyProductQuantity = productCondition?.Quantity ?? 0,
+                BuyProductIdentifier = productCondition?.Product?.Identifier,
+
+                BuyForFromCategory = amountCategoryCondition != null,
+                BuyForAmountFromCategory = categoryAmount,
+                BuyForCurrencyFromCategory = categoryAountCurrency?.Identifier,
+                BuyForIncludeTaxFromCategory = amountCategoryCondition?.TaxIncluded == true,
+                BuyForFromCategoryIdentifier = amountCategoryCondition?.Section?.Identifier,
+
+                BuyFromCategory = categoryCondition != null,
+                BuyFromCategoryQuantity = categoryCondition?.Quantity ?? 0,
+                BuyFromCategoryIdentifier = categoryCondition?.Section?.Identifier,
+            };
+
+            return conditionsModel;
+        }
+
+        public void MapToEnovaProperty(BaseObject obj, string propertyName, object value, IDictionary<string, object> otherValues)
+        {
+            var context = obj.GetContext();
+            var promo = (EnovaPromo)obj;
+            var amountCondition = promo.GetConditions(typeof(EnovaOrderAmountPromoCondition)).Cast<EnovaOrderAmountPromoCondition>().FirstOrDefault();
+            var productCondition = promo.GetConditions(typeof(EnovaProductQuantityPromoCondition)).Cast<EnovaProductQuantityPromoCondition>().FirstOrDefault();
+            var amountCategoryCondition = promo.GetConditions(typeof(EnovaSectionAmountPromoCondition)).Cast<EnovaSectionAmountPromoCondition>().FirstOrDefault();
+            var categoryCondition = promo.GetConditions(typeof(EnovaSectionQuantityPromoCondition)).Cast<EnovaSectionQuantityPromoCondition>().FirstOrDefault();
+
+            var conditionsModel = JsonConvert.DeserializeAnonymousType(value.ToString(), new {
+                BuyFor = false,
+                BuyForAmount = 0m,
+                BuyForAmountCurrency = String.Empty,
+                BuyForAmountIncludeTax = false,
+
+                BuyProduct = false,
+                BuyProductQuantity = 0d,
+                BuyProductIdentifier = String.Empty,
+
+                BuyForFromCategory = false,
+                BuyForAmountFromCategory = 0m,
+                BuyForCurrencyFromCategory = String.Empty,
+                BuyForIncludeTaxFromCategory = false,
+                BuyForFromCategoryIdentifier = String.Empty,
+
+                BuyFromCategory = false,
+                BuyFromCategoryQuantity = 0d,
+                BuyFromCategoryIdentifier = String.Empty
+            });
+
+            //BUY FOR
+            if(!conditionsModel.BuyFor && amountCondition != null)
+                promo.DeleteCondition(amountCondition);
+            else if(conditionsModel.BuyFor)
+            {
+                if (amountCondition == null)
+                {
+                    amountCondition = EnovaObjectCreationHelper.CreateNew<EnovaOrderAmountPromoCondition>(context);
+                    promo.AddCondition(amountCondition);
+                }
+                
+                if(String.IsNullOrEmpty(conditionsModel.BuyForAmountCurrency))
+                    amountCondition.SetAmount(conditionsModel.BuyForAmount);
+                else
+                    amountCondition.SetAmount(conditionsModel.BuyForAmount, EnovaCurrency.Find(context, conditionsModel.BuyForAmountCurrency));
+                amountCondition.TaxIncluded = conditionsModel.BuyForAmountIncludeTax;
+            }
+
+            //BUY PRODUCT
+            if (!conditionsModel.BuyProduct && productCondition != null)
+                promo.DeleteCondition(productCondition);
+            else if (conditionsModel.BuyProduct)
+            {
+                if (productCondition == null)
+                {
+                    productCondition = EnovaObjectCreationHelper.CreateNew<EnovaProductQuantityPromoCondition>(context);
+                    promo.AddCondition(productCondition);
+                }
+
+                productCondition.Quantity = conditionsModel.BuyProductQuantity;
+                productCondition.Product = context.FindObject<EnovaBaseProduct>(conditionsModel.BuyProductIdentifier);
+            }
+
+            //BUY AMOUNT FROM CATEGORY
+            if (!conditionsModel.BuyForFromCategory && amountCategoryCondition != null)
+                promo.DeleteCondition(amountCategoryCondition);
+            else if (conditionsModel.BuyForFromCategory)
+            {
+                if (amountCategoryCondition == null)
+                {
+                    amountCategoryCondition = EnovaObjectCreationHelper.CreateNew<EnovaSectionAmountPromoCondition>(context);
+                    promo.AddCondition(amountCategoryCondition);
+                }
+
+                amountCategoryCondition.TaxIncluded = conditionsModel.BuyForIncludeTaxFromCategory;
+
+                if (String.IsNullOrEmpty(conditionsModel.BuyForCurrencyFromCategory))
+                    amountCategoryCondition.SetAmount(conditionsModel.BuyForAmountFromCategory);
+                else
+                    amountCategoryCondition.SetAmount(conditionsModel.BuyForAmountFromCategory, EnovaCurrency.Find(context, conditionsModel.BuyForCurrencyFromCategory));
+                
+                amountCategoryCondition.Section = context.FindObject<EnovaBaseSection>(conditionsModel.BuyForFromCategoryIdentifier);
+            }
+
+            //BUY FROM CATEGORY
+            if (!conditionsModel.BuyFromCategory && categoryCondition != null)
+                promo.DeleteCondition(categoryCondition);
+            else if (conditionsModel.BuyFromCategory)
+            {
+                if (categoryCondition == null)
+                {
+                    categoryCondition = EnovaObjectCreationHelper.CreateNew<EnovaSectionQuantityPromoCondition>(context);
+                    promo.AddCondition(categoryCondition);
+                }
+
+                categoryCondition.Section = context.FindObject<EnovaBaseSection>(conditionsModel.BuyFromCategoryIdentifier);
+                categoryCondition.Quantity = conditionsModel.BuyFromCategoryQuantity;
+            }
+        }
+    }
+}
--- src/Wipcore.Enova.Api.WebApi/Mappers/Promo/PromoResultMapper.cs
+++ src/Wipcore.Enova.Api.WebApi/Mappers/Promo/PromoResultMapper.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Wipcore.Core.SessionObjects;
 using Wipcore.Enova.Api.Interfaces;
+using Wipcore.Enova.Api.WebApi.Helpers;
 using Wipcore.Enova.Core;
+using Wipcore.Enova.Generics;
 
 namespace Wipcore.eNova.Api.WebApi.Mappers.Promo
 {
@@ -15,63 +18,220 @@ namespace Wipcore.eNova.Api.WebApi.Mappers.Promo
         public bool InheritMapper => true;
 
         public int Priority => 0;
-        public MapType MapType => MapType.MapFromEnovaAllowed;
+        public MapType MapType => MapType.MapFromAndToEnovaAllowed;
 
         public object MapFromEnovaProperty(BaseObject obj, string propertyName)
         {
-            var promo = (EnovaPromo) obj; //TODO remake. match results to conditions, otherwise makes no sense in presentation
-            var results = new List<object>();
-            foreach (var enovaItem in promo.GetResults())
-            {
-                var decimals = 0;
-                if (enovaItem is EnovaFixedPricePromoResult)
-                {
-                    var item = (EnovaFixedPricePromoResult)enovaItem;
-                    results.Add(new
-                    {
-                        ID = item.ID,
-                        Price = item.GetPrice(out decimals),
-                        TaxIncluded = item.TaxIncluded
-                    });
-                }
-                else if (enovaItem is EnovaFreeProductPromoResult)
-                {
-                    var item = (EnovaFreeProductPromoResult)enovaItem;
-                    results.Add(new
-                    {
-                        ID = item.ID,
-                        ProductID = item.Product.ID,
-                        Quantity = item.Quantity
-                    });
-                }
-                else if (enovaItem is EnovaDiscountedProductPromoResult)
-                {
-                    var item = (EnovaDiscountedProductPromoResult)enovaItem;
-                    results.Add(new
-                    {
-                        ID = item.ID,
-                        Price = item.GetPrice(out decimals),
-                        Quantity = item.MaxQuantity,
-                        ProductID = item.Product.ID
-                    });
-                }
-                else if (enovaItem is EnovaProductQuantityPromoResult)
-                {
-                    var item = (EnovaProductQuantityPromoResult)enovaItem;
-                    results.Add(new
-                    {
-                        ID = item.ID,
-                        Quantity = item.Quantity
-                    });
-                }
-            }
+            var promo = (EnovaPromo) obj; 
+            var orderDiscountResult = promo.GetResults(typeof(EnovaOrderDiscountPromoResult)).Cast<EnovaOrderDiscountPromoResult>().FirstOrDefault();
+            var freeShippingResult = promo.GetResults(typeof(EnovaFreeShippingPromoResult)).Cast<EnovaFreeShippingPromoResult>().FirstOrDefault();
+            var productQuantityResult = promo.GetResults(typeof(EnovaProductQuantityPromoResult)).Cast<EnovaProductQuantityPromoResult>().FirstOrDefault();
+            var freeProductResult = promo.GetResults(typeof(EnovaFreeProductPromoResult)).Cast<EnovaFreeProductPromoResult>().FirstOrDefault();
+            var productDiscountResult = promo.GetResults(typeof(EnovaDiscountedProductPromoResult)).Cast<EnovaDiscountedProductPromoResult>().FirstOrDefault();
+            var fixedPriceResult = promo.GetResults(typeof(EnovaFixedPricePromoResult)).Cast<EnovaFixedPricePromoResult>().FirstOrDefault();
+            var sectionDiscount = promo.GetResults(typeof(EnovaSectionDiscountPromoResult)).Cast<EnovaSectionDiscountPromoResult>().FirstOrDefault();
 
-            return results;
+            Currency orderDiscountCurrency = null;
+            int decimals;
+            var orderDiscountAmount = orderDiscountResult == null ? 0m : orderDiscountResult.GetAmount(out decimals, ref orderDiscountCurrency);
+
+            Currency productDiscountCurrency = null;
+            var productDiscountPrice = productDiscountResult == null ? 0m : productDiscountResult.GetPrice(out decimals, ref productDiscountCurrency);
+
+            Currency fixedPriceCurrency = null;
+            var fixedPrice = fixedPriceResult == null ? 0m : fixedPriceResult.GetPrice(out decimals, ref fixedPriceCurrency);
+
+            var resultModel = new
+            {
+                OrderDiscount = orderDiscountResult != null,
+                OrderDiscountPercent = orderDiscountResult?.Percent ?? 0,
+                OrderDiscountAmount = orderDiscountAmount,
+                OrderDiscountCurrency = orderDiscountCurrency?.Identifier,
+                OrderDiscountIncludeTax = orderDiscountResult?.TaxIncluded == true,
+
+                FreeShipping = freeShippingResult != null,
+
+                AdditionalFreeProduct = productQuantityResult != null,
+                AdditionalFreeProductQuantity = productQuantityResult?.Quantity ?? 0d,
+
+                FreeProduct = freeProductResult != null,
+                FreeProductQuantity = freeProductResult?.Quantity ?? 0d,
+                FreeProductIdentifier = freeProductResult?.Product?.Identifier,
+
+                ProductDiscount = productDiscountResult != null,
+                ProductDiscountQuantity = productDiscountResult?.MaxQuantity ?? 0d,
+                ProductDiscountIdentifier = productDiscountResult?.Product?.Identifier,
+                ProductDiscountAmount = productDiscountPrice,
+                ProductDiscountCurrency = productDiscountCurrency?.Identifier,
+
+                FixedPrice = fixedPriceResult != null,
+                FixedPriceAmount = fixedPrice,
+                FixedPriceCurrency = fixedPriceCurrency?.Identifier,
+                FixedPriceIncludeTax = fixedPriceResult?.TaxIncluded == true,
+
+                SectionDiscount = sectionDiscount != null,
+                SectionDiscountPercent = sectionDiscount?.Percent ?? 0d,
+                SectionDiscountQuantity = sectionDiscount?.MaxQuantity ?? 0d,
+                SectionDiscountIdentifier = sectionDiscount?.Section?.Identifier
+            };
+
+            return resultModel;
         }
 
         public void MapToEnovaProperty(BaseObject obj, string propertyName, object value, IDictionary<string, object> otherValues)
         {
-            throw new NotImplementedException();
+            var context = obj.GetContext();
+            var promo = (EnovaPromo)obj;
+            var orderDiscountResult = promo.GetResults(typeof(EnovaOrderDiscountPromoResult)).Cast<EnovaOrderDiscountPromoResult>().FirstOrDefault();
+            var freeShippingResult = promo.GetResults(typeof(EnovaFreeShippingPromoResult)).Cast<EnovaFreeShippingPromoResult>().FirstOrDefault();
+            var productQuantityResult = promo.GetResults(typeof(EnovaProductQuantityPromoResult)).Cast<EnovaProductQuantityPromoResult>().FirstOrDefault();
+            var freeProductResult = promo.GetResults(typeof(EnovaFreeProductPromoResult)).Cast<EnovaFreeProductPromoResult>().FirstOrDefault();
+            var productDiscountResult = promo.GetResults(typeof(EnovaDiscountedProductPromoResult)).Cast<EnovaDiscountedProductPromoResult>().FirstOrDefault();
+            var fixedPriceResult = promo.GetResults(typeof(EnovaFixedPricePromoResult)).Cast<EnovaFixedPricePromoResult>().FirstOrDefault();
+            var sectionDiscount = promo.GetResults(typeof(EnovaSectionDiscountPromoResult)).Cast<EnovaSectionDiscountPromoResult>().FirstOrDefault();
+
+            var resultsModel = JsonConvert.DeserializeAnonymousType(value.ToString(), new
+            {
+                OrderDiscount = false,
+                OrderDiscountPercent = 0d,
+                OrderDiscountAmount = 0m,
+                OrderDiscountCurrency = String.Empty,
+                OrderDiscountIncludeTax = false,
+
+                FreeShipping = false,
+
+                AdditionalFreeProduct = false,
+                AdditionalFreeProductQuantity = 0d,
+
+                FreeProduct = false,
+                FreeProductQuantity = 0d,
+                FreeProductIdentifier = String.Empty,
+
+                ProductDiscount = false,
+                ProductDiscountQuantity = 0d,
+                ProductDiscountIdentifier = String.Empty,
+                ProductDiscountAmount = 0m,
+                ProductDiscountCurrency = String.Empty,
+
+                FixedPrice = false,
+                FixedPriceAmount = 0m,
+                FixedPriceCurrency = String.Empty,
+                FixedPriceIncludeTax = false,
+
+                SectionDiscount = false,
+                SectionDiscountPercent = 0d,
+                SectionDiscountQuantity = 0d,
+                SectionDiscountIdentifier = String.Empty
+            });
+
+            //ORDER DISCOUNT
+            if (!resultsModel.OrderDiscount && orderDiscountResult != null)
+                promo.DeleteResult(orderDiscountResult);
+            else if (resultsModel.OrderDiscount)
+            {
+                if (orderDiscountResult == null)
+                {
+                    orderDiscountResult = EnovaObjectCreationHelper.CreateNew<EnovaOrderDiscountPromoResult>(context);
+                    promo.AddResult(orderDiscountResult);
+                }
+
+                if (String.IsNullOrEmpty(resultsModel.OrderDiscountCurrency))
+                    orderDiscountResult.SetAmount(resultsModel.OrderDiscountAmount);
+                else
+                    orderDiscountResult.SetAmount(resultsModel.OrderDiscountAmount, EnovaCurrency.Find(context, resultsModel.OrderDiscountCurrency));
+                orderDiscountResult.TaxIncluded = resultsModel.OrderDiscountIncludeTax;
+                orderDiscountResult.Percent = resultsModel.OrderDiscountPercent;
+            }
+
+            //FREE SHIPPING
+            if (!resultsModel.FreeShipping && freeShippingResult != null)
+                promo.DeleteResult(freeShippingResult);
+            else if (resultsModel.FreeShipping)
+            {
+                if (freeShippingResult == null)
+                {
+                    freeShippingResult = EnovaObjectCreationHelper.CreateNew<EnovaFreeShippingPromoResult>(context);
+                    promo.AddResult(freeShippingResult);
+                }
+            }
+
+            //PRODUCT QUANTITY
+            if (!resultsModel.AdditionalFreeProduct && productQuantityResult != null)
+                promo.DeleteResult(productQuantityResult);
+            else if (resultsModel.AdditionalFreeProduct)
+            {
+                if (productQuantityResult == null)
+                {
+                    productQuantityResult = EnovaObjectCreationHelper.CreateNew<EnovaProductQuantityPromoResult>(context);
+                    promo.AddResult(productQuantityResult);
+                }
+                productQuantityResult.Quantity = resultsModel.AdditionalFreeProductQuantity;
+            }
+
+            //FREE PRODUCT
+            if (!resultsModel.FreeProduct && freeProductResult != null)
+                promo.DeleteResult(freeProductResult);
+            else if (resultsModel.FreeProduct)
+            {
+                if (freeProductResult == null)
+                {
+                    freeProductResult = EnovaObjectCreationHelper.CreateNew<EnovaFreeProductPromoResult>(context);
+                    promo.AddResult(freeProductResult);
+                }
+                freeProductResult.Quantity = resultsModel.FreeProductQuantity;
+                freeProductResult.Product = context.FindObject<EnovaBaseProduct>(resultsModel.FreeProductIdentifier);
+            }
+
+            //PRODUCT DISCOUNT
+            if (!resultsModel.ProductDiscount && productDiscountResult != null)
+                promo.DeleteResult(productDiscountResult);
+            else if (resultsModel.ProductDiscount)
+            {
+                if (productDiscountResult == null)
+                {
+                    productDiscountResult = EnovaObjectCreationHelper.CreateNew<EnovaDiscountedProductPromoResult>(context);
+                    promo.AddResult(productDiscountResult);
+                }
+                productDiscountResult.MaxQuantity = resultsModel.ProductDiscountQuantity;
+                productDiscountResult.Product = context.FindObject<EnovaBaseProduct>(resultsModel.ProductDiscountIdentifier);
+                if(String.IsNullOrEmpty(resultsModel.ProductDiscountCurrency))
+                    productDiscountResult.SetPrice(resultsModel.ProductDiscountAmount);
+                else
+                    productDiscountResult.SetPrice(resultsModel.ProductDiscountAmount, EnovaCurrency.Find(context, resultsModel.ProductDiscountCurrency));
+            }
+
+            //FIXED PRICE
+            if (!resultsModel.FixedPrice && fixedPriceResult != null)
+                promo.DeleteResult(fixedPriceResult);
+            else if (resultsModel.FixedPrice)
+            {
+                if (fixedPriceResult == null)
+                {
+                    fixedPriceResult = EnovaObjectCreationHelper.CreateNew<EnovaFixedPricePromoResult>(context);
+                    promo.AddResult(fixedPriceResult);
+                }
+                fixedPriceResult.TaxIncluded = resultsModel.FixedPriceIncludeTax;
+                if (String.IsNullOrEmpty(resultsModel.FixedPriceCurrency))
+                    fixedPriceResult.SetPrice(resultsModel.FixedPriceAmount);
+                else
+                    fixedPriceResult.SetPrice(resultsModel.FixedPriceAmount, EnovaCurrency.Find(context, resultsModel.FixedPriceCurrency));
+            }
+
+            //SECTION DISCOUNT
+            if (!resultsModel.SectionDiscount && sectionDiscount != null)
+                promo.DeleteResult(sectionDiscount);
+            else if (resultsModel.SectionDiscount)
+            {
+                if (sectionDiscount == null)
+                {
+                    sectionDiscount = EnovaObjectCreationHelper.CreateNew<EnovaSectionDiscountPromoResult>(context);
+                    promo.AddResult(sectionDiscount);
+                }
+                sectionDiscount.Percent = resultsModel.SectionDiscountPercent;
+                sectionDiscount.MaxQuantity = resultsModel.SectionDiscountQuantity;
+                sectionDiscount.Section = context.FindObject<EnovaBaseProductSection>(resultsModel.SectionDiscountIdentifier);
+            }
+
         }
     }
 }
