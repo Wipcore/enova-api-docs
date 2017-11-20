bb9729b10597f7be8724e2ed542e0dfff260d6cb
Henrik Eliasson <henrik.eliasson@wipcore.se>
2017-11-20 14:55:13 +0100

    Added more net client registrations

--- src/Wipcore.Enova.Api.NETClient/NetClientModule.cs
+++ src/Wipcore.Enova.Api.NETClient/NetClientModule.cs
@@ -7,6 +7,7 @@ using Wipcore.Enova.Api.Abstractions.Interfaces;
 using Wipcore.Enova.Api.Abstractions.Models.EnovaTypes.Cart;
 using Wipcore.Enova.Api.Abstractions.Models.EnovaTypes.Customer;
 using Wipcore.Enova.Api.Abstractions.Models.EnovaTypes.Order;
+using Wipcore.Enova.Api.Abstractions.Models.EnovaTypes.Product;
 using Wipcore.Enova.Api.NetClient;
 using Wipcore.Enova.Api.NETClient;
 
@@ -21,7 +22,13 @@ namespace Wipcore.Enova.Api.NetClient
             builder.RegisterType<ApiRepositoryAsync>().AsImplementedInterfaces().SingleInstance();
             builder.RegisterType<ApiRepository>().AsImplementedInterfaces().SingleInstance();
             builder.RegisterType<CartRepository<CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<CartRepositoryAsync<CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
             builder.RegisterType<CustomerRepository<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<CustomerRepositoryAsync<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<ProductRepository<ProductModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<ProductRepositoryAsync<ProductModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<OrderRepository<OrderModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<OrderRepositoryAsync<OrderModel>>().AsImplementedInterfaces().SingleInstance();
         }
 
         public int Priority => 0;
