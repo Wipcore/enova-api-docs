048e3158fa6438c05bf9b821a178931dc5ce6a53
Henrik Eliasson <henrik.eliasson@wipcore.se>
2017-11-21 09:35:24 +0100

    Added a new autofac module for dependencies registrations

---
+++ src/Wipcore.Enova.Api.NETClient/NetClientDependenciesModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Wipcore.Enova.Api.Abstractions.Models.EnovaTypes.Product;
+using Wipcore.Enova.Api.NetClient;
+
+namespace Wipcore.Enova.Api.NETClient
+{
+    /// <summary>
+    /// This class registers all components that are needed to use NetClient. NOTE: only use this module if you do not want to setup all registrations yourself,
+    /// as this can otherwise overwrite better registrations.
+    /// </summary>
+    public class NetClientDependenciesModule : Module
+    {
+       
+        protected override void Load(ContainerBuilder builder)
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("netclient.json");
+            var configRoot = configBuilder.Build();
+
+            var servicecollection = new ServiceCollection();
+            servicecollection.AddDataProtection();
+            var serviceProvider = servicecollection.BuildServiceProvider();
+            
+            builder.RegisterInstance<IConfigurationRoot>(configRoot).SingleInstance();
+            builder.RegisterInstance<ILoggerFactory>(new LoggerFactory()).SingleInstance();
+            builder.RegisterInstance<IServiceProvider>(serviceProvider).SingleInstance();
+            builder.RegisterType<HttpContextAccessor>().AsSelf().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterInstance<IDataProtectionProvider>(serviceProvider.GetDataProtectionProvider()).SingleInstance();
+        }
+    }
+}
