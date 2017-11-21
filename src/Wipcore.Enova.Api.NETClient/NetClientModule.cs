d3f0f4e5caca45b400833fe10999fa0a89654fed
Henrik Eliasson <henrik.eliasson@wipcore.se>
2017-11-21 09:33:44 +0100

    Register repositories as self

--- src/Wipcore.Enova.Api.NETClient/NetClientModule.cs
+++ src/Wipcore.Enova.Api.NETClient/NetClientModule.cs
@@ -17,18 +17,18 @@ namespace Wipcore.Enova.Api.NetClient
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<ApiClientAsync>().AsImplementedInterfaces();
-            builder.RegisterType<ApiClient>().AsImplementedInterfaces();
-            builder.RegisterType<ApiRepositoryAsync>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<ApiRepository>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<CartRepository<CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<CartRepositoryAsync<CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<CustomerRepository<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<CustomerRepositoryAsync<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<ProductRepository<ProductModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<ProductRepositoryAsync<ProductModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<OrderRepository<OrderModel>>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<OrderRepositoryAsync<OrderModel>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<ApiClientAsync>().AsImplementedInterfaces().AsSelf();
+            builder.RegisterType<ApiClient>().AsImplementedInterfaces().AsSelf();
+            builder.RegisterType<ApiRepositoryAsync>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<ApiRepository>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<CartRepository<CartModel, OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<CartRepositoryAsync<CartModel, OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<CustomerRepository<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<CustomerRepositoryAsync<CustomerModel, CartModel, OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<ProductRepository<ProductModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<ProductRepositoryAsync<ProductModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<OrderRepository<OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
+            builder.RegisterType<OrderRepositoryAsync<OrderModel>>().AsImplementedInterfaces().AsSelf().SingleInstance();
         }
 
         public int Priority => 0;
