2237ba66aa85357e93f30b97df329e4e36ab51a5
Henrik Eliasson <henrik.eliasson@wipcore.se>
2018-03-22 16:38:06 +0100

    Added modelstate error check and error message

---
+++ src/Wipcore.Enova.Api.Abstractions.Internal/HttpException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace Wipcore.Enova.Api.Abstractions.Internal
+{
+    /// <summary>
+    /// A HttpException with statuscode and message.
+    /// </summary>
+    public class HttpException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+
+        public HttpException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
