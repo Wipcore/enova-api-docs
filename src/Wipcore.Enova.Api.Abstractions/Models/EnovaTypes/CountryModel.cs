c162d1a9ed96ebcf851f88b395e1f562203a98c7
Henrik Eliasson <henrik.eliasson@wipcore.se>
2018-03-22 16:38:39 +0100

    Minor countrymodel fixes, namespace, groupattribute

---
+++ src/Wipcore.Enova.Api.Abstractions/Models/EnovaTypes/CountryModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Wipcore.Enova.Api.Abstractions.Attributes;
+
+namespace Wipcore.Enova.Api.Abstractions.Models.EnovaTypes
+{
+    [IndexModel]
+    [GroupPresentation("Country", new string[] { "Identifier", "Name" }, sortOrder: 100)]
+    public class CountryModel : BaseModel
+    {
+        [PropertyPresentation("String", null, isEditable: true, isFilterable: true, isGridColumn: true, sortOrder: 100)]
+        public string Name { get; set; }
+
+        public override List<string> GetDefaultPropertiesInGrid() => new List<string>() { "Identifier", "Name", "ModifiedAt" };
+
+        public override string GetResourceName() => "countries";
+    }
+}
