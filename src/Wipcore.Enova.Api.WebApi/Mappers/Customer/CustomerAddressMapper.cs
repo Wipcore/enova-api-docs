a6a9c8a70703e1b534bde8dc02008ec13262c6c5
Daniel Gustavsson <daniel.gustavsson@wipcore.se>
2018-04-10 14:22:18 +0200

    Feature/gdpr adjustments (#23)
    * Added a base template for all the customer information for admin to print
    
    * Added a mapper for CustomerAddress
    
    * Added a CustomerCartMapper class. You can now see the carts and the items in the carts belonging to the specified customer
    
    * Added more mapping properties to CustomerAddressMapper and cleaned up some code in CustomerCartsMapper
    
    * Refactored code to be more unilateral
    
    * Reverted local settings in appsettings
    
    * Changed CustomerAddress to plural
    
    * CustomerAddress now also return Id. Also refactored logic for retreiving AddressType
    
    * Cart now returns Id on both cart and products in cart. Also refactored a few variables
    
    * CustomerAddressMapper now retreives addresses using the method GetAddresses. Changed key in Dictionary in CustomerCartsMapper to Product from Item
    
    * Removed commented code

---
+++ src/Wipcore.Enova.Api.WebApi/Mappers/Customer/CustomerAddressMapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wipcore.Core.SessionObjects;
+using Wipcore.Enova.Api.Abstractions.Internal;
+using Wipcore.Enova.Core;
+
+namespace Wipcore.Enova.Api.WebApi.Mappers.Customer
+{
+    public class CustomerAddressMapper : IPropertyMapper
+    {
+        public List<string> Names => new List<string>() { "CustomerAddresses" };
+        public Type Type => typeof(EnovaCustomer);
+        public bool InheritMapper => true;
+        public int Priority => 0;
+        public MapType MapType => MapType.MapFromEnovaAllowed;
+        public bool PostSaveSet => false;
+        public bool FlattenMapping => false;
+        public object GetEnovaProperty(BaseObject obj, string propertyName, List<EnovaLanguage> languages)
+        {
+            var customer = (EnovaCustomer)obj;
+            var addresses = new List<object>();
+            foreach (var address in customer.GetAddresses(typeof(EnovaCustomerAddress)).OfType<EnovaCustomerAddress>())
+            {
+                var miniAddress = new Dictionary<string, object>
+                {
+                    {"Id", address.ID },
+                    {"Identifier", address.Identifier},
+                    {"Street", address.Street },
+                    {"PostalCode", address.PostalCode },
+                    {"City", address.City },
+                    {"Country", address.CountryName },
+                    {"Phone", address.Phone },
+                    {"AddressType", ((EnovaCustomerAddress.AddressTypeEnum)address.AddressType).ToString() }
+                };
+                addresses.Add(miniAddress);
+            }
+            return addresses;
+        }
+
+        public void SetEnovaProperty(BaseObject obj, string propertyName, object value, IDictionary<string, object> otherValues)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Wipcore.Enova.Api.WebApi/Mappers/Customer/CustomerCartsMapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wipcore.Core.SessionObjects;
+using Wipcore.Enova.Api.Abstractions.Internal;
+using Wipcore.Enova.Core;
+using Wipcore.Enova.Generics;
+
+namespace Wipcore.Enova.Api.WebApi.Mappers.Customer
+{
+    public class CustomerCartsMapper : IPropertyMapper
+    {
+        public List<string> Names => new List<string>() { "CustomerCarts" };
+        public Type Type => typeof(EnovaCustomer);
+        public bool InheritMapper => true;
+        public int Priority => 0;
+        public MapType MapType => MapType.MapFromEnovaAllowed;
+        public bool PostSaveSet => false;
+        public bool FlattenMapping => false;
+        public object GetEnovaProperty(BaseObject obj, string propertyName, List<EnovaLanguage> languages)
+        {
+            var customer = (EnovaCustomer)obj;
+            var carts = new List<object>();
+
+            foreach (var cart in customer.GetCarts(typeof(EnovaCart)).Cast<EnovaCart>()) 
+            {
+                var cartProducts = new List<object>();
+                foreach (var product in cart.GetCartItems<EnovaProductCartItem>())
+                {
+                    var miniCartProduct = new Dictionary<string, object>
+                    {
+                        { "ID", product.ID },
+                        { "Identifier", product.Product.Identifier },
+                        { "Name", product.Product.Name },
+                        { "Price", product.Product.MainPrice }
+                    };
+                    cartProducts.Add(miniCartProduct);
+                }
+
+                var miniCart = new Dictionary<string, object>()
+                {
+                    {"Id", cart.ID },
+                    {"Identifier", cart.Identifier},
+                    {"CreatedAt", cart.CreatedAt },
+                    {"Products", cartProducts }
+                };
+                carts.Add(miniCart);
+            }
+            return carts;
+        }
+
+        public void SetEnovaProperty(BaseObject obj, string propertyName, object value, IDictionary<string, object> otherValues)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
